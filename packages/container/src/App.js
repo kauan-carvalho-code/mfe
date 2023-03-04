077dd3f224f8c5495a563695126be3f2c8796c0d
Kauan Carvalho <kauan.carvalho@memed.com.br>
2023-03-04 00:57:12 -0300

    some changes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
   return (
     <div>
-      <h1>Container!</h1>
+      <h1>Hello Container!</h1>
 
       <MarketingApp />
     </div>
